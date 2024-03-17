be6b17a6390fccc0359d7ac1a754da1be6bea19f
ninz0018 <ninzgetil25@gmail.com>
2024-03-17 21:13:14 +0800

    fonlab

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="bg-dark">
+    <nav id="navbar" class="fw-bold navbar navbar-dark navbar-expand-md">
+                <a class="ms-3 navbar-brand" href="#">
+                    <img style="height: 60px;" src="1.png"></img>
+                </a>
+                    <button style="margin-right: 10px;" class="btn-warning navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                <div  class="align-self-end justify-content-end p-3 collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item">
+                            <a class="fa nav-link text-white text-decoration-none" href="#">Login</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="fa nav-link text-white text-decoration-none" href="#proj">Signup</a>
+                    </ul>
+                </div>
+            </nav>
+    </div>
+    
+</body>
+</html>
+
