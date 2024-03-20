12b526848e9cef2ff6b0b3c4f337899e3ffda17a
ninz0018 <ninzgetil25@gmail.com>
2024-03-19 16:29:40 -0800

    fonelab

--- login/signinSuccss.php
+++ login/signinSuccss.php
@@ -9,9 +9,73 @@
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+    <link rel="stylesheet" href="style.css">
     <title>FONELAB</title>
 </head>
-<body class="bg-dark" >
-    
+<body>
+    <div class="border bg-white rounded fa d-flex justify-content-center align-items-center" >
+        <h1>CELLPHONE ACCESSORIES</h1>
+    </div>
+    <div class="bg-white row p-3">
+        <div class="col-9 border rounded">
+            <div class="fw-bold d-flex justify-content-center align-items-center border rounded">
+                <h3>CHARGER</h3>
+            </div>
+            <div class=" col-12 border rounded d-flex justify-content-center align-items-center" >
+                <div class="view col-2 border rounded p-1 ">
+                    <h5 class="d-flex justify-content-center align-items-center">Type C Charger</h5>
+                    <a class="d-flex justify-content-center align-items-center" href="" > <img style="height: 100px;" class="rounded" src="../product/1.jpg"  alt=""></a>
+                    <div class="d-flex justify-content-center align-items-center">
+                        <button class="btn btn-primary rounded mt-2 ">Add to Cart</button>
+                    </div>
+                </div>
+                <div class="view col-2 border rounded p-1 ">
+                    <h5 class="d-flex justify-content-center align-items-center">Type C Charger</h5>
+                    <a class="d-flex justify-content-center align-items-center" href="" > <img style="height: 100px;" class="rounded" src="../product/2.jpg"  alt=""></a>
+                    <div class="d-flex justify-content-center align-items-center">
+                        <button class="btn btn-primary rounded mt-2 ">Add to Cart</button>
+                    </div>
+                </div>
+                <div class="view col-2 border rounded p-1 ">
+                    <h5 class="d-flex justify-content-center align-items-center">Type C Charger</h5>
+                    <a class="d-flex justify-content-center align-items-center" href="" > <img style="height: 100px;" class="rounded" src="../product/1.jpg"  alt=""></a>
+                    <div class="d-flex justify-content-center align-items-center">
+                        <button class="btn btn-primary rounded mt-2 ">Add to Cart</button>
+                    </div>
+                </div>
+                <div class="view col-2 border rounded p-1 ">
+                    <h5 class="d-flex justify-content-center align-items-center">Type C Charger</h5>
+                    <a class="d-flex justify-content-center align-items-center" href="" > <img style="height: 100px;" class="rounded" src="../product/1.jpg"  alt=""></a>
+                    <div class="d-flex justify-content-center align-items-center">
+                        <button class="btn btn-primary rounded mt-2 ">Add to Cart</button>
+                    </div>
+                </div>
+                <div class="view col-2 border rounded p-1 ">
+                    <h5 class="d-flex justify-content-center align-items-center">Type C Charger</h5>
+                    <a class="d-flex justify-content-center align-items-center" href="" > <img style="height: 100px;" class="rounded" src="../product/1.jpg"  alt=""></a>
+                    <div class="d-flex justify-content-center align-items-center">
+                        <button class="btn btn-primary rounded mt-2 ">Add to Cart</button>
+                    </div>
+                </div>
+                <div class="view col-2 border rounded p-1 ">
+                    <h5 class="d-flex justify-content-center align-items-center">Type C Charger</h5>
+                    <a class="d-flex justify-content-center align-items-center" href="" > <img style="height: 100px;" class="rounded" src="../product/1.jpg"  alt=""></a>
+                    <div class="d-flex justify-content-center align-items-center">
+                        <button class="btn btn-primary rounded mt-2 ">Add to Cart</button>
+                    </div>
+                </div>
+                
+                
+            </div>
+        </div>
+        <div class="col border rounded" >
+            <div class="d-flex justify-content-center align-items-center">
+                <h3>ORDER LIST</h3>
+            </div>
+            <ul>
+                <li>First</li>
+            </ul>
+        </div>
+    </div>
 </body>
 </html>
